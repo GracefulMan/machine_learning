e893409250459c66e33e84a83d8fe90d117ee6d9
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-06-07 01:07:56 +0800

    change epoch

--- myvae.py
+++ myvae.py
@@ -6,25 +6,24 @@ from keras.layers import Conv2D, UpSampling2D,Conv2DTranspose
 from keras.models import Model
 from keras import backend as K
 from keras.losses import mse, binary_crossentropy
-from keras.datasets import mnist
-from skimage import io
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-num_conv = 7
+from keras.datasets import mnist, cifar10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+num_conv = 8
 batch_size = 100
-intermediate_dim = 128
+intermediate_dim = 512
 epochs = 25
 pic_size = x_train.shape[1]
 latent_dim = pic_size * pic_size
 epsilon_std = 1.0
-dim_ = 1
-x_train = np.reshape(x_train, [-1, pic_size, pic_size])
-x_test = np.reshape(x_test, [-1, pic_size, pic_size])
+dim_ = 3
+x_train = np.reshape(x_train, [-1, pic_size, pic_size, dim_])
+x_test = np.reshape(x_test, [-1, pic_size, pic_size, dim_])
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 #np.save('x_test.npy',x_test)
 print(x_train.shape,x_test.shape)
-x = Input(batch_shape=(batch_size,pic_size, pic_size,))
-conv_1 = Reshape((pic_size,pic_size,1))(x)
+x = Input(batch_shape=(batch_size,pic_size, pic_size,dim_))
+conv_1 = Reshape((pic_size,pic_size,dim_))(x)
 conv_1 = Conv2D(1, kernel_size=num_conv,padding='same', activation='relu')(conv_1)
 conv_2 = Conv2D(64, kernel_size=num_conv,padding='same', strides=2, activation='relu')(conv_1)
 conv_3 = Conv2D(64, kernel_size=num_conv,padding='same', activation='relu')(conv_2)
@@ -39,15 +38,16 @@ def sampling(args):
     return(z_mean + K.exp(z_log_var/2) * epsilon)
 z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
 decoder_h = Dense(256, activation='relu')(z)
-decoder = Dense(pic_size * pic_size, activation='relu')(decoder_h)
-decoder = Reshape((pic_size, pic_size,1))(decoder)
+decoder = Dense(pic_size * pic_size*dim_, activation='relu')(decoder_h)
+print(decoder.shape)
+decoder = Reshape((pic_size, pic_size,dim_))(decoder)
 de_conv_1 = Conv2DTranspose(64, kernel_size=num_conv,padding='same', activation='relu')(decoder)
 de_conv_2 = Conv2DTranspose(64, kernel_size=num_conv,padding='same', activation='relu')(de_conv_1)
 #upsamp = UpSampling2D(2)(de_conv_2)
-x_decoded_mean = Conv2DTranspose(1, kernel_size=num_conv,
+x_decoded_mean = Conv2DTranspose(3, kernel_size=num_conv,
                         padding='same', activation='relu')(de_conv_2)
 print(x_decoded_mean.shape)
-x_decoded_mean = Reshape([pic_size, pic_size])(x_decoded_mean)
+x_decoded_mean = Reshape([pic_size, pic_size,dim_] )(x_decoded_mean)
 
 def vae_loss(x, x_decoded_mean,loss_type = 'mse'):
     if loss_type == 'mse':
@@ -68,4 +68,4 @@ vae.fit(x_train,
         validation_data=(x_test, None))
 vae.save('myvae.h5')
 res = vae.predict(x_test,batch_size=batch_size)
-np.save('test1.npy',res)
+np.save('test2.npy',res)
