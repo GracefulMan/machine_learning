102fe4f0ce5928deb26832c93f79dab2b789e273
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-06-18 02:25:31 +0800

    change epoch

--- dog_vae.py
+++ dog_vae.py
@@ -70,7 +70,7 @@ def vae_loss(x, x_decoded_mean,loss_type = 'mse'):
                                                   K.flatten(x_decoded_mean))
     reconstruction_loss *= pic_size * pic_size
     kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) -K.exp(z_log_var), axis=-1)
-    return reconstruction_loss + kl_loss
+    return K.mean(reconstruction_loss + kl_loss)
 vae = Model(x, x_decoded_mean)
 #plot_model(vae, to_file='my_vae_cnn.png', show_shapes=True)
 vae.summary()
