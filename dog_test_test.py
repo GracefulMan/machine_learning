c06ae074f0c2d0863b883d8ff7f65ffae275fb61
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-06-18 03:49:59 +0800

    change epoch

--- dog_test_test.py
+++ dog_test_test.py
@@ -12,7 +12,7 @@ noise_factor = 0.05
 def load_data():
     train_data = np.load('dog_train.npy')
     test_data = np.load('dog_test.npy')
-    return train_data[:1000,:,:],test_data[:1000,:,:]
+    return train_data[:1500,:,:],test_data[:1000,:,:]
 x_train , x_test = load_data()
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
