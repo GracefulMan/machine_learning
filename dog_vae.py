4aae9f28efe106e2a4bc34162d113bccc0845856
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-06-18 01:45:26 +0800

    change epoch

--- dog_vae.py
+++ dog_vae.py
@@ -12,6 +12,8 @@ def load_data():
     test_data = np.load('dog_test.npy')
     return train_data,test_data
 x_train , x_test = load_data()
+x_train = x_train[:1760,:,:]
+x_test = x_train[:1247,:,:]
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
